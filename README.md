8fffa67f453b7ecd155f87f7fda0cc4ce2be86c7
Code by Ben <code.by.ben@gmail.com>
2024-09-16 08:35:00 +1000

    readme and tests.

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # archive
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/Defacto2/archive.svg)](https://pkg.go.dev/github.com/Defacto2/archive)
+
 Provides compressed and stored archive file extraction and content listing.
+
+## Usage
+
+In your Go project, import the releaser library.
+
+```sh
+go get github.com/Defacto2/archive
+```
+
+Use the functions.
+
+```go
+import "github.com/Defacto2/archive"
+
+func main() {
+Source    // Extract all files from an archive.
+    if err := archive.Extract("path/to/archive.zip", "path/to/extract"); err != nil {
+        fmt.Println(err)
+    }
+
+    // Extract a specific files from an archive.
+    x := archive.Extractor{
+        Source: "path/to/archive.zip",
+        Destination: "path/to/extract",
+    }
+    if err := x.Extract("file1.txt", "file2.txt"); err != nil {
+        fmt.Println(err)
+    }
+
+    // Extract all files to a temporary directory.
+    path, err := archive.ExtractSource("path/to/archive.zip", "tempsubdir")
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Extracted to:", path)
+
+    // List the contents of an archive.
+    files, err := archive.List("path/to/archive.zip", "archive.zip")
+    if err != nil {
+        fmt.Println(err)
+    }
+    for _, f := range files {
+        fmt.Println(f)
+    }
+
+    // Search for a possible readme file within the list of files.
+    name := archive.Readme("archive.zip", cont.Files)
+    fmt.Println(name)
+
+    // Compress a file into a new archive.
+    if _, err := rezip.Compress("file1.txt", "path/to/new.zip"); err != nil {
+        fmt.Println(err)
+    }
+    // Compress a directory into a new archive.
+    if _, err = rezip.CompressDir("path/to/directory", "path/to/new.zip"); err != nil {
+        fmt.Println(err)
+    }
+}
+```
---
+++ archive_test.go
@@ -0,0 +1,87 @@
+package archive_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/Defacto2/archive"
+	"github.com/Defacto2/archive/rezip"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUsage(t *testing.T) {
+	t.Parallel()
+
+	zipfile := "testdata/PKZ80A1.ZIP"
+	zipfalse := "testdata/test.zip"
+	dst := filepath.Join(os.TempDir(), "archive_test")
+
+	err := archive.ExtractAll(zipfalse, os.TempDir())
+	require.Error(t, err)
+	err = archive.ExtractAll(zipfile, dst)
+	defer os.RemoveAll(dst)
+	require.NoError(t, err)
+
+	x := archive.Extractor{
+		Source:      zipfalse,
+		Destination: dst,
+	}
+	err = x.Extract()
+	require.Error(t, err)
+
+	x = archive.Extractor{
+		Source:      zipfile,
+		Destination: dst,
+	}
+	err = x.Extract()
+	defer os.RemoveAll(dst)
+	require.NoError(t, err)
+
+	path, err := archive.ExtractSource(zipfalse, "archive_test")
+	require.Error(t, err)
+	require.Empty(t, path)
+
+	path, err = archive.ExtractSource(zipfile, "")
+	defer os.RemoveAll(path)
+	require.NoError(t, err)
+	assert.NotEmpty(t, path)
+
+	files, err := archive.List(zipfalse, "test.zip")
+	require.Error(t, err)
+	require.Empty(t, files)
+
+	files, err = archive.List(zipfile, "PKZ80A1.ZIP")
+	require.NoError(t, err)
+	assert.Len(t, files, 15)
+
+	name := archive.Readme(zipfalse, "test.zip")
+	require.Empty(t, name)
+
+	name = archive.Readme(zipfile, "PKZ80A1.ZIP")
+	assert.Empty(t, name)
+
+	_, err = rezip.Compress(zipfalse, dst)
+	require.Error(t, err)
+
+	toComp := "testdata/TEST.EXE"
+	dstComp := filepath.Join(os.TempDir(), "archive_test.zip")
+	_ = os.Remove(dstComp)
+
+	_, err = rezip.Compress(toComp, dstComp)
+	require.NoError(t, err)
+	_, err = rezip.Compress(toComp, dstComp)
+	require.Error(t, err)
+	_ = os.Remove(dstComp)
+
+	_, err = rezip.CompressDir(zipfalse, dstComp)
+	require.Error(t, err)
+	_ = os.Remove(dstComp)
+	_, err = rezip.CompressDir("testdata", dstComp)
+	require.NoError(t, err)
+	_, err = rezip.CompressDir("testdata", dstComp)
+	require.Error(t, err)
+	_ = os.Remove(dstComp)
+
+}
